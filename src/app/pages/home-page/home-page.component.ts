9059c48ab0a5c5d3cbf6a29ecb5fbd0b45a106b6
Michał <mike.projektowanie@gmail.com>
2023-11-13 12:51:59 +0100

    add product-list to homepage

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductsListComponent } from '../../components/products-list/products-list.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ProductsListComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
-export class HomePageComponent {
-
-}
+export class HomePageComponent {}
